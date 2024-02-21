75ffa1a7730df9381a7bdfae2b9b7fc78c06dc2d
katia <katiafalletti97@gmail.com>
2024-02-21 16:51:22 +0100

    primo commit stickerstacker

---
+++ script.js
@@ -0,0 +1,168 @@
+const grid = document.querySelector('.grid');
+const stackBtn = document.querySelector('.stack')
+const scoreCounter = document.querySelector('.score-counter');
+
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+//0 cella vuota
+//1 barra
+
+const gridMatrix = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0]
+
+];
+
+let currentRowIndex = gridMatrix.length - 1;
+let barDirection = 'right';
+let barSize = 3;
+let isGameOver = false;
+let score = 0;
+let t;
+
+function draw() {
+    grid.innerHTML = '';
+    gridMatrix.forEach(function (rowContent, rowIndex) {
+        rowContent.forEach(function (cellContent, cellIndex) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            const isRowEven = rowIndex % 2 === 0;
+            const isCellEven = cellIndex % 2 === 0;
+
+            if ((isRowEven && isCellEven) || (!isRowEven && !isCellEven)) {
+                cell.classList.add('cell-dark');
+            }
+
+            if (cellContent === 1) {
+                cell.classList.add('bar');
+            }
+
+            grid.appendChild(cell)
+        })
+    })
+}
+
+function moveRight(row) {
+    row.pop();
+    row.unshift(0);
+}
+
+function moveLeft(row) {
+    row.shift();
+    row.push(0);
+}
+
+function isRightEdge(row) {
+    const lastElement = row[row.length - 1];
+    return lastElement === 1;
+}
+
+function isLeftEdge(row) {
+    const firstElement = row[0];
+    return firstElement === 1;
+}
+
+function moveBar() {
+    const currentRow = gridMatrix[currentRowIndex];
+
+    if (barDirection === 'right') {
+        moveRight(currentRow);
+
+        if (isRightEdge(currentRow)) {
+            barDirection = 'left';
+        }
+
+    } else if (barDirection === 'left') {
+        moveLeft(currentRow);
+        if (isLeftEdge(currentRow)) {
+            barDirection = 'right';
+        }
+    }
+}
+function checklost() {
+    const currentRow = gridMatrix[currentRowIndex];
+    const prevRow = gridMatrix[currentRowIndex + 1];
+
+    if (!prevRow) return;
+
+    for (let i = 0; i < currentRow.length; i++) {
+        if (currentRow[i] === 1 && prevRow[i] === 0) {
+            currentRow[i] = 0;
+            barSize--;
+        }
+    }
+
+    if (barSize === 0) {
+        isGameOver = true;
+        clearInterval(t);
+        endGame(false);
+    }
+}
+
+function checkWin() {
+    if (currentRowIndex === 0) {
+        isGameOver = true;
+        clearInterval(t);
+        endGame(true);
+    }
+}
+
+function onStack() {
+    //checklost
+    checklost();
+    //checkwin
+    checkWin();
+    //agiorno il punteggio
+    updateScore();
+
+    if (isGameOver) return;
+
+    //cambioriga
+    currentRowIndex--;
+    barDirection = 'right';
+    for (let i = 0; i < barSize; i++) {
+        gridMatrix[currentRowIndex][i] = 1;
+    }
+    draw();
+}
+
+function updateScore() {
+    score++;
+    scoreCounter.innerText = score.toString().padStart(5, '0');
+
+    /*total score
+    const finalBlock = document.querySelectorAll('.bar');*/
+
+}
+
+function endGame(isVictory) {
+    if (isVictory) {
+        endGameText.innerHTML = 'YOU <br> WIN';
+        endGameScreen.classList.add('win');
+    }
+    endGameScreen.classList.remove('hidden');
+}
+
+draw();
+
+function main() {
+    moveBar()
+    draw()
+}
+
+stackBtn.addEventListener('click', onStack);
+
+playAgainButton.addEventListener('click', function () {
+    location.reload();
+});
+
+t = setInterval(main, 1000);
